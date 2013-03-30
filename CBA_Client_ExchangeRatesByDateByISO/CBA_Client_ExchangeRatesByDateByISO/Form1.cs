0207bc298d554315ed31b04f704d52a00de17737
Ashot Karapetyan <karapetyan.ash@gmail.com>
2013-03-31 00:01:51 +0400

    initial commit for .NET WSDL projects

---
+++ CBA_Client_ExchangeRatesByDateByISO/CBA_Client_ExchangeRatesByDateByISO/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CBA_Client_ExchangeRatesByDateByISO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.selectedISO.Items.AddRange(Program.getAvailableCurrencies());
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+
+            this.renderTable();
+            /*
+            Thread thread = new Thread(new ThreadStart(this.renderTable));
+            thread.Start();*/
+            
+        }
+
+        private  void renderTable()
+        {
+            //Form1 this_ = (Form1)form;
+            this.selectedISO.Enabled = false;
+            this.fromDateTimePicker.Enabled = false;
+            this.toDateTimePicker.Enabled = false;
+            this.submitButton.Enabled = false;
+            DateTime from = this.fromDateTimePicker.Value;
+            DateTime to = this.toDateTimePicker.Value;
+            String selectedISO = (String)this.selectedISO.SelectedItem;
+            IEnumerator<CBA.ExchangeRatesDS.ExchangeRatesByRangeRow> enumerator = Program.getRates(selectedISO, from, to);
+            int rowIndex = 0;
+            while (enumerator.MoveNext())
+            {
+                CBA.ExchangeRatesDS.ExchangeRatesByRangeRow current = enumerator.Current;
+                int ammount = current.Amount;
+                String iso = current.ISO;
+                decimal diff = current.Diff;
+                decimal rate = current.Rate;
+                String date = current.RateDate.ToString("dd/MM/yy");
+                this.ratesTable.Rows.Add(new String[] { date, rate.ToString(), diff.ToString() });
+                Color backColor = diff > 0 ? Color.Green : Color.Red;
+                this.ratesTable.Rows[rowIndex++].Cells[2].Style.BackColor = backColor;
+            }
+
+            this.selectedISO.Enabled = true;
+            this.fromDateTimePicker.Enabled = true;
+            this.toDateTimePicker.Enabled = true;
+            this.submitButton.Enabled = true;
+        }
+
+      
+    }
+}
---
+++ CBA_Client_ExchangeRatesByDateByISO/CBA_Client_ExchangeRatesByDateByISO/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace CBA_Client_ExchangeRatesByDateByISO
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+  
+
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        public static IEnumerator<CBA.ExchangeRatesDS.ExchangeRatesByRangeRow> getRates(String isoCodec, DateTime from, DateTime to)
+        {
+            CBA.GateSoapClient client = new CBA.GateSoapClient();
+            client.Open();
+
+            
+            CBA_Client_ExchangeRatesByDateByISO.CBA.ExchangeRatesDS.ExchangeRatesByRangeDataTable
+                ratesTable = client.ExchangeRatesByDateRangeByISO(isoCodec, from, to);
+            IEnumerator<CBA.ExchangeRatesDS.ExchangeRatesByRangeRow> enumerator = ratesTable.GetEnumerator();
+            return enumerator;
+            /*
+                        while (enumerator.MoveNext())
+                        {
+                            CBA.ExchangeRatesDS.ExchangeRatesByRangeRow current = enumerator.Current;
+                            int ammount  = current.Amount;
+                            String iso = current.ISO;
+                            decimal diff = current.Diff;
+                            decimal rate = current.Rate;
+
+                        }
+             * */
+        }
+
+
+        public static String[] getAvailableCurrencies()
+        {
+            CBA.GateSoapClient client = new CBA.GateSoapClient();
+            client.Open();
+
+
+            return  client.ISOCodes();
+            
+        }
+    }
+}
